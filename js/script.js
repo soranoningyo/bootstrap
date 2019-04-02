371912f193f0f7aa4f5c3d4ff3fc661ceb3bd10c
sora <sora0no0ningyo@gmail.com>
2019-04-02 21:56:51 +0200

    testimonial was added using custom carousel

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 /*global $ ,document ,window */
-$('.carousel').carousel({
+// main caruosel
+
+$('body > .carousel').carousel({
 
     interval: 2500
 
 });
 $(document).ready(function () {
+
     'use strict';
+
     $(".carousel-inner > .item > img, .carousel-inner > .item > a > img").css("height", $(window).height());
+
 });
 $(window).resize(function () {
+
     'use strict';
+
     $(".carousel-inner > .item > img, .carousel-inner > .item > a > img").css("height", $(window).height());
+
+});
+// carousel of testimonial
+$('#Testimonialslider').carousel({
+
+    interval: 2500
+
 });
