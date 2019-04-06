88cfa4712be7bd89d972bef5a593c32c983d1f46
sora <sora0no0ningyo@gmail.com>
2019-04-06 16:14:23 +0200

    onload page and color box were added

--- js/script.js
+++ js/script.js
@@ -32,4 +32,27 @@ $('#Testimonialslider').carousel({
     interval: 2500
 
 });
-
+// select-box
+$('.gear-icon').click(function () {
+$('.selection-box').toggleClass('in');
+});
+//
+$('.selection-box .color-box ul li:first-of-type').click(function () {
+    $(':root').css('--maincolors', '#27ae62');
+});
+$('.selection-box .color-box ul li:nth-of-type(2)').click(function () {
+    $(':root').css('--maincolors', '#c33e26');
+});
+$('.selection-box .color-box ul li:nth-of-type(3)').click(function () {
+    $(':root').css('--maincolors', '#2778ae');
+});
+$('.selection-box .color-box ul li:nth-of-type(4)').click(function () {
+    $(':root').css('--maincolors', '#ae2744');
+});
+window.onload = function () {
+    'use strict';
+    $('.loading').delay(500).fadeOut(1500, function () {
+        $('body').css('overflow', 'auto');
+        $(this).remove();
+    });
+};
